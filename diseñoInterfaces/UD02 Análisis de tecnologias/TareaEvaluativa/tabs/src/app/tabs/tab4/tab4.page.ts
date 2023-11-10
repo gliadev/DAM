1f8e9bef4fc930c420f165a6cfb7ef6d292c9bc2
gliadev <gliatem@gmail.com>
2023-11-10 12:04:03 +0100

    terminada tareaEvaluativa02

--- diseñoInterfaces/UD02 Análisis de tecnologias/TareaEvaluativa/tabs/src/app/tabs/tab4/tab4.page.ts
+++ diseñoInterfaces/UD02 Análisis de tecnologias/TareaEvaluativa/tabs/src/app/tabs/tab4/tab4.page.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-
-  constructor() { }
+  private numeroAdivinar!: number;
+  public mensaje: string = '';
+  public numeroIntroducido: number | null = null;
 
   ngOnInit() {
+    this.numeroAdivinar = Math.floor(Math.random() * 101);
   }
 
+  comprobarNumero() {
+    if (this.numeroIntroducido !== null) {
+      if (this.numeroIntroducido < 0 || this.numeroIntroducido > 100) {
+        this.mensaje = 'Introduce un número entre 0 y 100';
+      } else if (this.numeroIntroducido < this.numeroAdivinar) {
+        this.mensaje = 'Introduce un número mayor';
+      } else if (this.numeroIntroducido > this.numeroAdivinar) {
+        this.mensaje = 'Introduce un número menor';
+      } else {
+        this.mensaje = '¡Has acertado!';
+      }
+    } else {
+      this.mensaje = 'Por favor, introduce un número';
+    }
+  }
 }
