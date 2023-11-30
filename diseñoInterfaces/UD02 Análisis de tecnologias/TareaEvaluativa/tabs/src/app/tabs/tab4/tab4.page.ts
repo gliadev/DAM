444bdd1ded374381b730d23a71f5e52082c44ffb
gliadev <gliatem@gmail.com>
2023-12-01 00:11:08 +0100

    subibiendo DI02

--- diseñoInterfaces/UD02 Análisis de tecnologias/TareaEvaluativa/tabs/src/app/tabs/tab4/tab4.page.ts
+++ diseñoInterfaces/UD02 Análisis de tecnologias/TareaEvaluativa/tabs/src/app/tabs/tab4/tab4.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
@@ -9,13 +10,27 @@ export class Tab4Page implements OnInit {
   private numeroAdivinar!: number;
   public mensaje: string = '';
   public numeroIntroducido: number | null = null;
+  public juegoTerminado: boolean = false;
+  public intentos: number = 0;
+
+  constructor(private alertController: AlertController) {}
 
   ngOnInit() {
-    this.numeroAdivinar = Math.floor(Math.random() * 101);
+    this.resetearJuego();
   }
 
-  comprobarNumero() {
+  resetearJuego() {
+    this.numeroAdivinar = Math.floor(Math.random() * 101);
+    this.numeroIntroducido = null;
+    this.juegoTerminado = false;
+    this.mensaje = '';
+    this.intentos = 0;
+  }
+
+  async comprobarNumero() {
     if (this.numeroIntroducido !== null) {
+      this.intentos++;
+
       if (this.numeroIntroducido < 0 || this.numeroIntroducido > 100) {
         this.mensaje = 'Introduce un número entre 0 y 100';
       } else if (this.numeroIntroducido < this.numeroAdivinar) {
@@ -23,10 +38,34 @@ export class Tab4Page implements OnInit {
       } else if (this.numeroIntroducido > this.numeroAdivinar) {
         this.mensaje = 'Introduce un número menor';
       } else {
-        this.mensaje = '¡Has acertado!';
+        this.mensaje = `Numero de Intentos: ${this.intentos}`;
+        this.mensaje = `¡Enhorabuena!! Has acertado el numero secreto`;
+        this.juegoTerminado = true;
+        this.preguntarVolverAJugar();
       }
     } else {
       this.mensaje = 'Por favor, introduce un número';
     }
+  }
+
+  async preguntarVolverAJugar() {
+    const alert = await this.alertController.create({
+      header: 'Nuevo juego',
+      message: '¿Quieres volver a jugar?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'OK',
+          handler: () => {
+            this.resetearJuego();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
 }
